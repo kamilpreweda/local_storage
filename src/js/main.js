177c476cc56149fb5f14fbccbce3d33b2473daf3
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-25 17:03:18 +0200

    created addItem function

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,22 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
 
-console.log(`Hello world!`);
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  this.reset();
+}
+
+addItems.addEventListener('submit', addItem);
+
 
 
